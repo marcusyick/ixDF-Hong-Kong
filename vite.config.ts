195e49091e2266922aae0ba616e768f851cdacbf
marcusyick <marcusyick@gmail.com>
2025-11-21 19:33:05 +0800

    deploy v9

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
